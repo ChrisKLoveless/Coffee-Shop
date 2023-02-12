190b488f0ec5593d040b47cee09f32be36aa8d0f
ChrisKLoveless <chriskloveless@gmail.com>
2023-02-12 11:35:09 -0800

    WIP: button to sell pound not working properly

--- src/components/Coffee.jsx
+++ src/components/Coffee.jsx
@@ -5,10 +5,14 @@ function Coffee(props) {
   return (
     <React.Fragment>
       <div onClick = {() => props.whenCoffeeClicked(props.id)}>
-      <h3>{props.origin} - {props.name}</h3>
-      <p><em>{props.price} - {props.roast}</em></p>
+      <h3>Origin: {props.origin}</h3>
+      <h3>Name: {props.name}</h3> 
+      <p><em>Roast: {props.roast}</em></p>
+      <p><em>Price: $ {props.price} / lb</em></p>
+      <p><em>Inventory: {props.pounds}lbs</em></p>
       <hr />
       </div>
+      <button onClick={ () => props.whenSellPoundClicked(props.id) } id={props.id}> Sell Pound </button>
     </React.Fragment>
   );
 }
@@ -18,8 +22,10 @@ Coffee.propTypes = {
   origin: PropTypes.string,
   price: PropTypes.string,
   roast: PropTypes.string,
+  pounds: PropTypes.number,
   id: PropTypes.string,
-  whenCoffeeClicked: PropTypes.func
+  whenCoffeeClicked: PropTypes.func,
+  whenSellPoundClicked: PropTypes.func
 };
 
 export default Coffee;
